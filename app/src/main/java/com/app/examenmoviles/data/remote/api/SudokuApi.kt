d3af52f480f5e3ea05f9d94abe6ac579e1e68d5e
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-11-27 09:36:45 -0600

    [UPDATE] Data layer

---
+++ app/src/main/java/com/app/examenmoviles/data/remote/api/SudokuApi.kt
@@ -0,0 +1,17 @@
+package com.app.examenmoviles.data.remote.api
+
+import com.app.examenmoviles.data.remote.dto.SudokuDto
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface SudokuApi{
+    @GET("sudokugenerate")
+    suspend fun getSudokuGame(
+        @Header("X-Api-Key") apiKey: String,
+        @Query("width") width: Int = 4,
+        @Query("height") height: Int = 4,
+        @Query("difficulty") difficulty: String = "easy",
+    ): SudokuDto
+}
