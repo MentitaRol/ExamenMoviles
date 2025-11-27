1395a4ff5652815d42947896f2b7bc2ad9fa5b99
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-11-27 00:44:23 -0600

    [UPDATE] Hilt and Manifest configuration

---
+++ app/src/main/java/com/app/examenmoviles/data/remote/dto/Dto.kt
@@ -0,0 +1,2 @@
+package com.app.examenmoviles.data.remote.dto
+
--- app/src/main/java/com/app/examenmoviles/presentation/MainActivity.kt
+++ app/src/main/java/com/app/examenmoviles/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.app.examenmoviles
+package com.app.examenmoviles.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.app.examenmoviles.presentation.theme.ExamenMovilesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +22,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ExamenMovilesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExamenMovilesTheme {
-        Greeting("Android")
     }
 }
